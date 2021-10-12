ba31bc400ca72a6c00632cd38a2569aee9409509
Cyril Dutrieux (cydu) <cydu@odoo.com>
2021-10-12 12:01:33 +0200

    Implement the rest of the functionality

--- env_var.sh
+++ env_var.sh
@@ -1,4 +1,7 @@
 export OSCOUT_RG_PREFIX='rg --color=always --column --with-filename'
-export OSCOUT_EDITCMD='kcr edit {1} +{2}:{3}'
-export OSCOUT_REGEX=''
-export OSCOUT_PATHS=''
+export OSCOUT_PATH=''
+export OSCOUT_TMP=$(mktemp -d)
+export ARGS=$@
+
+export OSCOUT_EDITCMD='bat {1}  --pager="less +{2}"'
+#export OSCOUT_EDITCMD='subl {1}:{2}:{3} '
--- run_utils.sh
+++ run_utils.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 reload_command () {
-    echo ${OSCOUT_HEADER};
-    echo "${OSCOUT_RG_PREFIX} ${OSCOUT_RG_TYPE_FILTER} \"${OSCOUT_REGEX}${1}\" ${OSCOUT_PATHS} ${ARGS} || true";
-    eval "${OSCOUT_RG_PREFIX} ${OSCOUT_RG_TYPE_FILTER} \"${OSCOUT_REGEX}${1}\" ${OSCOUT_PATHS} ${ARGS} || true";
+    echo "$(get_header)";
+    echo "${OSCOUT_RG_PREFIX} $(get_type_filter) \"$(get_prefix_regex)${1}\" ${OSCOUT_PATHS} ${ARGS} || true";
+    eval "${OSCOUT_RG_PREFIX} $(get_type_filter) \"$(get_prefix_regex)${1}\" ${OSCOUT_PATHS} ${ARGS} || true";
 };
 
-toggle_type () {
+store () {
+    echo ${1} > "$OSCOUT_TMP/$2"
+};
+
+retrieve () {
+    path="${OSCOUT_TMP}/${1}"
+    if [[ -f $path ]]
+    then
+        echo $(<$path)
+    fi
+};
+
+toggle () {
     new_ls=()
     removed=0
+    OSCOUT_SEARCH_TYPES=($(retrieve "$2"))
     for st in $OSCOUT_SEARCH_TYPES
     do
         if [ "$st" = "$1" ]
@@ -21,17 +34,45 @@ toggle_type () {
     then
         new_ls+=("$1")
     fi
-    export OSCOUT_SEARCH_TYPES=("$new_ls[@]")
-    export OSCOUT_RG_TYPE_FILTER=$(generate_type_filter)
+    store "$new_ls" "$2"
 };
 
-generate_type_filter () {
+get_type_filter () {
+    OSCOUT_SEARCH_TYPES=($(retrieve "type"))
     s=""
     for st in $OSCOUT_SEARCH_TYPES
     do
         s+="-t$st "
     done
     echo $s
+};
+
+get_prefix_regex () {
+    SEARCH_PREFIX=($(retrieve "prefix"))
+    if [[ ${#SEARCH_PREFIX[@]} -eq 0 ]]
+    then
+        exit
+    fi
+    s="($(prefix_list))\s*=?\s*('|\\\")?"
+    echo $s
+};
+
+prefix_list () {
+    s=""
+    sep=""
+    for st in $SEARCH_PREFIX
+    do
+        s+="$sep$st"
+        sep="|"
+    done
+    echo $s
+};
+
+get_header () {
+    OSCOUT_SEARCH_TYPES=($(retrieve "type"))
+    SEARCH_PREFIX=($(retrieve "prefix"))
+    echo "Matching : $(prefix_list)"
+    echo "Looking for files : $OSCOUT_SEARCH_TYPES"
 };
 
 if [[ -f ~/.config/oscout/run_utils.sh ]]
