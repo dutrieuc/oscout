4c6ac3a834ec4486ff6f1ea1d976a5c74ab36854
Cyril Dutrieux (cydu) <cydu@odoo.com>
2021-10-13 18:41:57 +0200

    Refactor and remove useless definitions

--- env_var.sh
+++ env_var.sh
@@ -1,10 +1,7 @@
-export OSCOUT_RG_PREFIX='rg --color=always --column --with-filename --smart-case'
-export OSCOUT_PATH=''
+export OSCOUT_RG_OPTS='--color=always --smart-case'
 export OSCOUT_TMP=$(mktemp -d)
 export ARGS=$@
 
-export OSCOUT_EDITCMD='bat {1}  --pager="less +{2}"'
-#export OSCOUT_EDITCMD='subl {1}:{2}:{3} '
 
 if [[ -f $HOME/.config/oscout/env_var.sh ]]
 then
